769099d91fab2de52e9aa4b441cadaa988a204ac
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 10:32:15 -0600

    iterate through the given list

--- algo.js
+++ algo.js
@@ -2,4 +2,10 @@
 //  Code Wars: FILTERED LIST
 //////////////////////////////////////////////////////
 
+function filter_list(l) {
+    for (let x=0; x<l.length; x++) {
+        console.log (typeof(l[x]);)
+    }
+}
 
+filter_list([1,2,'a','b']);
