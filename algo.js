583d974083f1c50976043a5d5214c2b603acec7c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 10:44:06 -0600

    find number type

--- algo.js
+++ algo.js
@@ -3,8 +3,12 @@
 //////////////////////////////////////////////////////
 
 function filter_list(l) {
+    let newist = [];
     for (let x=0; x<l.length; x++) {
-        console.log (typeof(l[x]);)
+        console.log (typeof(l[x]));
+        if (typeof(l[x]) == "number") {
+            console.log("number found");
+        }
     }
 }
 
