990a3d8a61908652a66e71c3c0d573ac1011543c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 11:01:54 -0600

    add test 3

--- algo.js
+++ algo.js
@@ -14,4 +14,4 @@ function filter_list(l) {
 
 console.log("Filter [1,2,'a','b']:",filter_list([1,2,'a','b']));
 console.log("Filter [1,'a','b',0,15]:",filter_list([1,'a','b',0,15]));
-// console.log("Filter [1,2,'a','b']:",filter_list([1,2,'a','b']));
+console.log("Filter [1,2,'aasf','1','123',123]:",filter_list([1,2,'aasf','1','123',123]));
